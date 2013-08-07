f6addb3fe13b589c13a93b5134458626b4e9fed0
Volodymyr <yfshaolin@gmail.com>
2013-08-07 19:29:21 +0300

    changed GetMessage() - PeekMessage()

--- 01_begin/App.cpp
+++ 01_begin/App.cpp
@@ -1,18 +1,24 @@
 #include "Engine\GraphicsStarter.h"
 
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCommandLine, int nCommandShow)
 {
+    
 	Window appWindow = Window(hInstance, nCommandShow);
     GraphicsStarter graphics = GraphicsStarter(appWindow);
 
 	graphics.CreateGraphics3D(800, 600);
 
     MSG msg;
-    while(GetMessage(&msg, NULL, 0, 0))
+    while(TRUE)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if(msg.message == WM_QUIT)
+                break;
+        }
     }
     return msg.wParam;
 }
